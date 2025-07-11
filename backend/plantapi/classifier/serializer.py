656b7db259f21f9bc5d3f0530d1d96ec5de9b8ed
Johnatan <johrasamoelina@gmail.com>
2025-07-11 10:04:59 +0300

    added : serializer , urls and views , waiting for .pkl

--- backend/plantapi/classifier/serializer.py
+++ backend/plantapi/classifier/serializer.py
@@ -1,2 +1,5 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 
+class PlantInputSerializer(serializers.Serializer):
+    humidity_level = serializers.ChoiceField(choices=["élevé", "moyen", "faible"])
+    isYellowLeaves = serializers.BooleanField()
